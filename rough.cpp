00a8e7025fc2db76cc66c21003405b12350dfafe
Saikumar242002 <113327016+Saikumar242002@users.noreply.github.com>
2022-09-16 10:29:47 +0530

    Add files via upload

---
+++ rough.cpp
@@ -0,0 +1,7 @@
+printf(" %i\t\t\t", i+1);
+		printf("%i\t\t\t\t", processSize[i]);
+		if (allocation[i] != -1)
+			printf("%i", allocation[i] + 1);
+		else
+			printf("Not Allocated");
+		printf("\n");
